8d114cc450d85d4e7b4e3c0d61b5b4033c56563c
reactivegeeks <software.dev0218@gmail.com>
2019-03-21 15:17:44 +0000

    search

--- src/controllers/trips.js
+++ src/controllers/trips.js
@@ -227,6 +227,24 @@ exports.searchDestination = (req,res) => {
     }
 	};
 
+	//search gender ---
+	req.query['gender'] != undefined ? query['gender'] = req.query['gender'] : undefined;
+
+	//search surf modality ---
+	req.query['surf_modality'] != undefined ? query['surf_modality'] = req.query['surf_modality'] : undefined;
+
+	//search surf level ---
+	req.query['surf_level'] != undefined ? query['surf_level'] = req.query['surf_level'] : undefined;
+
+	//search transport ---
+	req.query['transport'] != undefined ? query['transport'] = req.query['transport'] : undefined;
+
+	//search accomodation ---
+	req.query['accomodation'] != undefined ? query['accomodation'] = req.query['accomodation'] : undefined;
+	
+	//search max no. surfers ---
+	req.query['number_of_surfers'] != undefined ? query['number_of_surfers'] = { $lte: req.query['number_of_surfers'] }  : undefined;
+
 	Trip.find(query).skip(skip).limit(50).then(trips => {
 		res.json(trips);
 	}).catch(err => {
