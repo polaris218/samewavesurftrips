e29e5e2e68418f7a21d4bab438de9c63871b9cf5
reactivegeeks <software.dev0218@gmail.com>
2019-05-28 23:12:16 +1000

    update Static build, add Homepage ref on package.json

---
+++ static/app/precache-manifest.1a72b88099156db69dc31cece25850a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2476368647f8cc96ff8",
+    "url": "/static/css/main.aefccdc9.chunk.css"
+  },
+  {
+    "revision": "f2476368647f8cc96ff8",
+    "url": "/static/js/main.f2476368.chunk.js"
+  },
+  {
+    "revision": "c7426eaf97095476ef0c",
+    "url": "/static/css/1.58c9c1d2.chunk.css"
+  },
+  {
+    "revision": "c7426eaf97095476ef0c",
+    "url": "/static/js/1.c7426eaf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "491c3bc53a663345f7b639accb15b9ca",
+    "url": "/static/media/background_van.491c3bc5.jpg"
+  },
+  {
+    "revision": "d37d86689a502bbbad8cfbabe4aec027",
+    "url": "/static/media/icon_fb.d37d8668.svg"
+  },
+  {
+    "revision": "7f44ca8cb202ed38074cdfe452f46962",
+    "url": "/index.html"
+  }
+];
