3e7a6d0d57e61a3c8f45d859fc5cffdd2feb3e6b
reactivegeeks <software.dev0218@gmail.com>
2019-05-28 23:26:01 +1000

    reconfigure app root

--- static/app/service-worker.js
+++ static/app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a72b88099156db69dc31cece25850a6.js"
+  "/app/precache-manifest.2fb2ffeb5e596d693401c096fe90aaba.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
