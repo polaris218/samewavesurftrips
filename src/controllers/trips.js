4e03577fcc0962cf4ed4757dddfd462e7f62ff1b
reactivegeeks <software.dev0218@gmail.com>
2019-09-06 13:24:46 +0530

    By default display trips whose return date are equal to today or greater

--- src/controllers/trips.js
+++ src/controllers/trips.js
@@ -211,17 +211,17 @@ exports.search = (req, res) => {
       "$gte": new Date(req.query['departure_date_time'])
     }) : undefined;
 
-    if(!req.query['departure_date_time']) {
-      query['date_times.departure_date_time'] = {
-        "$gte": new Date().setHours(0, 0, 0, 0)
-      }
-    }
-
   //search return_date_time ---
   req.query['return_date_time'] != undefined || '' ?
     (query['date_times.return_date_time'] = {
       "$lte": new Date(req.query['return_date_time']).setHours(23, 59, 59, 0)
     }) : undefined;
+
+  if (!req.query['return_date_time']) {
+    query['date_times.return_date_time'] = {
+      "$gte": new Date().setHours(23, 59, 59, 0)
+    }
+  }
 
   //search title ---
   req.query['title'] != undefined ? (query['title'] = new RegExp(`.*${req.query['title']}.*`, 'i')) : undefined;
